ed970605051796409f5adddd11049e2b2b9abc44
ConniBug <connorbilham9@gmail.com>
2021-08-03 19:30:50 +0100

    Changes to be committed: 	modified:   ass.js

--- ass.js
+++ ass.js
@@ -33,7 +33,7 @@ log.blank().info(`* ${ASS_NAME} v${ASS_VERSION} *`).blank();
 
 // Set up premium frontend
 const FRONTEND_NAME = 'conni-ass-frontend'; // <-- Change this to use a custom frontend
-const ASS_PREMIUM = fs.existsSync(`./${FRONTEND_NAME}/package.json`) ? (require('submodule'), require(`./${FRONTEND_NAME}`)) : { enabled: false };
+const ASS_PREMIUM = fs.existsSync(`./${FRONTEND_NAME}/package.json`) ? (require(`./${FRONTEND_NAME}`)) : { enabled: false };
 
 //#region Variables, module setup
 const app = express();
